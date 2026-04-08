d735bcd3c1d3aa244755bd179fd263440310ad2a
sebha01 <30077548@students.southwales.ac.uk>
2026-04-08 13:40:08 +0100

    Sorted out navbar spacing for right side and added in images folder from old portfolio

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,12 +5,12 @@ function NavBar() {
     return (
         <nav className="navbar">
           <div className="navbar-left">
-            <Link to="/">Home</Link> | {" "}
+            <Link to="/">Home</Link>
           </div>
           <div className="navbar-center">
-            <Link to="/projects">Projects</Link> | {" "}
-            <Link to="/about">About Me</Link> | {" "}
-            <Link to="/contact">Contact Me</Link> | {" "}
+            <Link to="/projects">Projects</Link>
+            <Link to="/about">About Me</Link>
+            <Link to="/contact">Contact Me</Link>
           </div>
         </nav>
     )
